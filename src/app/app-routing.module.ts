42ca705ecdb036e4835a2fac14788e86a0a91568
REN <rendy1429@gmail.com>
2018-04-06 04:21:06 +0700

    add new page login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent }
 ];
 
