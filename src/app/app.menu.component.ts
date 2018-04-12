1727d1eed9323aee84602bfd7d8865591fef917d
MRR\Rizki <mr.rmdhn6@gmail.com>
2018-04-13 00:58:38 +0700

    dashoard page

---
+++ src/app/app.menu.component.ts
@@ -0,0 +1,232 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {trigger, state, style, transition, animate} from '@angular/animations';
+import {Location} from '@angular/common';
+import {Router} from '@angular/router';
+import {MenuItem} from 'primeng/primeng';
+import {AppComponent} from './app.component';
+
+@Component({
+    selector: 'app-menu',
+    template: `
+        <ul app-submenu [item]="model" root="true" class="navigation-menu" visible="true"></ul>
+    `
+})
+export class AppMenuComponent implements OnInit {
+
+    public model: any[];
+
+    constructor(public app: AppComponent) {}
+
+    ngOnInit() {
+        this.model = [
+            {label: 'Dashboard', icon: 'dashboard', routerLink: ['/']},
+            {
+                label: 'Themes', icon: 'palette', badge: '5',
+                items: [
+                    {label: 'Blue - Amber', icon: 'brush', command: (event) => {this.changeTheme('blue'); }},
+                    {label: 'Teal - Amber', icon: 'brush', command: (event) => {this.changeTheme('teal'); }},
+                    {label: 'Blue Grey - Green', icon: 'brush', command: (event) => {this.changeTheme('blue-grey'); }},
+                    {label: 'Cyan - Yellow', icon: 'brush', command: (event) => {this.changeTheme('cyan'); }},
+                    {label: 'Dark - Blue', icon: 'brush', command: (event) => {this.changeTheme('dark-blue'); }},
+                    {label: 'Dark - Green', icon: 'brush', command: (event) => {this.changeTheme('dark-green'); }},
+                    {label: 'Light Blue - Green', icon: 'brush', command: (event) => {this.changeTheme('light-blue'); }},
+                    {label: 'Indio - Cyan', icon: 'brush', command: (event) => {this.changeTheme('indigo'); }},
+                    {label: 'Deep Purple - Pink', icon: 'brush', command: (event) => {this.changeTheme('deep-purple'); }},
+                    {label: 'Green - Yellow', icon: 'brush', command: (event) => {this.changeTheme('green'); }}
+                ]
+            },
+            {
+                label: 'Menu Modes', icon: 'settings_application',
+                items: [
+                    {label: 'Static Menu', icon: 'menu',  command: () => this.app.changeToStaticMenu()},
+                    {label: 'Overlay Menu', icon: 'exit_to_app',  command: () => this.app.changeToOverlayMenu()},
+                    {label: 'Light Menu', icon: 'label_outline',  command: () => this.app.darkMenu = false},
+                    {label: 'Dark Menu', icon: 'label',  command: () => this.app.darkMenu = true}
+                ]
+            },
+            {
+                label: 'Components', icon: 'list', badge: '2', badgeStyleClass: 'red-badge',
+                items: [
+                    {label: 'Sample Page', icon: 'desktop_mac', routerLink: ['/sample']},
+                    {label: 'Forms', icon: 'input', routerLink: ['/forms']},
+                    {label: 'Data', icon: 'grid_on', routerLink: ['/data']},
+                    {label: 'Panels', icon: 'content_paste', routerLink: ['/panels']},
+                    {label: 'Overlays', icon: 'content_copy', routerLink: ['/overlays']},
+                    {label: 'Menus', icon: 'menu', routerLink: ['/menus']},
+                    {label: 'Messages', icon: 'message', routerLink: ['/messages']},
+                    {label: 'Charts', icon: 'insert_chart', routerLink: ['/charts']},
+                    {label: 'File', icon: 'attach_file', routerLink: ['/file']},
+                    {label: 'Misc', icon: 'toys', routerLink: ['/misc']}
+                ]
+            },
+            {
+                label: 'Template Pages', icon: 'get_app',
+                items: [
+                    {label: 'Empty Page', icon: 'hourglass_empty', routerLink: ['/empty']},
+                    {label: 'Landing Page', icon: 'flight_land', url: 'assets/pages/landing.html', target: '_blank'},
+                    {label: 'Login Page', icon: 'verified_user', url: 'assets/pages/login.html', target: '_blank'},
+                    {label: 'Error Page', icon: 'error', url: 'assets/pages/error.html', target: '_blank'},
+                    {label: '404 Page', icon: 'error_outline', url: 'assets/pages/404.html', target: '_blank'},
+                    {label: 'Access Denied Page', icon: 'security', url: 'assets/pages/access.html', target: '_blank'}
+                ]
+            },
+            {
+                label: 'Menu Hierarchy', icon: 'menu',
+                items: [
+                    {
+                        label: 'Submenu 1', icon: 'subject',
+                        items: [
+                            {
+                                label: 'Submenu 1.1', icon: 'subject',
+                                items: [
+                                    {label: 'Submenu 1.1.1', icon: 'subject'},
+                                    {label: 'Submenu 1.1.2', icon: 'subject'},
+                                    {label: 'Submenu 1.1.3', icon: 'subject'},
+                                ]
+                            },
+                            {
+                                label: 'Submenu 1.2', icon: 'subject',
+                                items: [
+                                    {label: 'Submenu 1.2.1', icon: 'subject'},
+                                    {label: 'Submenu 1.2.2', icon: 'subject'}
+                                ]
+                            },
+                        ]
+                    },
+                    {
+                        label: 'Submenu 2', icon: 'subject',
+                        items: [
+                            {
+                                label: 'Submenu 2.1', icon: 'subject',
+                                items: [
+                                    {label: 'Submenu 2.1.1', icon: 'subject'},
+                                    {label: 'Submenu 2.1.2', icon: 'subject'},
+                                    {label: 'Submenu 2.1.3', icon: 'subject'},
+                                ]
+                            },
+                            {
+                                label: 'Submenu 2.2', icon: 'subject',
+                                items: [
+                                    {label: 'Submenu 2.2.1', icon: 'subject'},
+                                    {label: 'Submenu 2.2.2', icon: 'subject'}
+                                ]
+                            },
+                        ]
+                    }
+                ]
+            },
+            {label: 'Utils', icon: 'build', routerLink: ['/utils']},
+            {label: 'Documentation', icon: 'find_in_page', routerLink: ['/documentation']}
+        ];
+    }
+
+    changeTheme(theme) {
+        const themeLink: HTMLLinkElement = <HTMLLinkElement> document.getElementById('theme-css');
+        const layoutLink: HTMLLinkElement = <HTMLLinkElement> document.getElementById('layout-css');
+
+        themeLink.href = 'assets/theme/theme-' + theme + '.css';
+        layoutLink.href = 'assets/layout/css/layout-' + theme + '.css';
+    }
+}
+
+@Component({
+    /* tslint:disable:component-selector */
+    selector: '[app-submenu]',
+    /* tslint:enable:component-selector */
+    template: `
+        <ng-template ngFor let-child let-i="index" [ngForOf]="(root ? item : item.items)">
+            <li [ngClass]="{'active-menuitem': isActive(i)}" [class]="child.badgeStyleClass" *ngIf="child.visible === false ? false : true">
+                <a [href]="child.url||'#'" (click)="itemClick($event,child,i)" class="ripplelink"
+                   *ngIf="!child.routerLink" [attr.tabindex]="!visible ? '-1' : null" [attr.target]="child.target"
+                    (mouseenter)="hover=true" (mouseleave)="hover=false">
+                    <i class="material-icons">{{child.icon}}</i>
+                    <span>{{child.label}}</span>
+                    <span class="ink" *ngIf="hover"></span>
+                    <span class="menuitem-badge" *ngIf="child.badge">{{child.badge}}</span>
+                    <i class="material-icons" *ngIf="child.items">keyboard_arrow_down</i>
+                </a>
+
+                <a (click)="itemClick($event,child,i)" class="ripplelink" *ngIf="child.routerLink"
+                    [routerLink]="child.routerLink" routerLinkActive="active-menuitem-routerlink"
+                   [routerLinkActiveOptions]="{exact: true}" [attr.tabindex]="!visible ? '-1' : null" [attr.target]="child.target"
+                    (mouseenter)="hover=true" (mouseleave)="hover=false">
+                    <i class="material-icons">{{child.icon}}</i>
+                    <span>{{child.label}}</span>
+                    <span class="ink" *ngIf="hover"></span>
+                    <span class="menuitem-badge" *ngIf="child.badge">{{child.badge}}</span>
+                    <i class="material-icons" *ngIf="child.items">keyboard_arrow_down</i>
+                </a>
+                <ul app-submenu [item]="child" *ngIf="child.items" [@children]="isActive(i) ?
+                'visible' : 'hidden'" [visible]="isActive(i)"></ul>
+            </li>
+        </ng-template>
+    `,
+    animations: [
+        trigger('children', [
+            state('hidden', style({
+                height: '0px'
+            })),
+            state('visible', style({
+                height: '*'
+            })),
+            transition('visible => hidden', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)')),
+            transition('hidden => visible', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)'))
+        ])
+    ]
+})
+export class AppSubMenuComponent {
+
+    @Input() item: MenuItem;
+
+    @Input() root: boolean;
+
+    @Input() visible: boolean;
+
+    activeIndex: number;
+
+    hover: boolean;
+
+    constructor(public app: AppComponent, public router: Router, public location: Location) {}
+
+    itemClick(event: Event, item: MenuItem, index: number) {
+        // avoid processing disabled items
+        if (item.disabled) {
+            event.preventDefault();
+            return true;
+        }
+
+        // activate current item and deactivate active sibling if any
+        this.activeIndex = (this.activeIndex === index) ? null : index;
+
+        // execute command
+        if (item.command) {
+            item.command({originalEvent: event, item: item});
+        }
+
+        // prevent hash change
+        if (item.items || (!item.url && !item.routerLink)) {
+            event.preventDefault();
+        }
+
+        // hide menu
+        if (!item.items && this.app.overlay) {
+            this.app.sidebarActive = false;
+        }
+    }
+
+    isActive(index: number): boolean {
+        return this.activeIndex === index;
+    }
+
+    unsubscribe(item: any) {
+        if (item.eventEmitter) {
+            item.eventEmitter.unsubscribe();
+        }
+
+        if (item.items) {
+            for (const childItem of item.items) {
+                this.unsubscribe(childItem);
+            }
+        }
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,105 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import 'rxjs/add/operator/toPromise';
+
+// Import Component
 import { ButtonModule } from 'primeng/primeng';
 import { AppComponent } from './app.component';
+import { AccordionModule } from 'primeng/primeng';
+import { AutoCompleteModule } from 'primeng/primeng';
+import { BreadcrumbModule } from 'primeng/primeng';
+import { CalendarModule } from 'primeng/primeng';
+import { CarouselModule } from 'primeng/primeng';
+import { ColorPickerModule } from 'primeng/primeng';
+import { ChartModule } from 'primeng/primeng';
+import { CheckboxModule } from 'primeng/primeng';
+import { ChipsModule } from 'primeng/primeng';
+import { CodeHighlighterModule } from 'primeng/primeng';
+import { ConfirmDialogModule } from 'primeng/primeng';
+import { SharedModule } from 'primeng/primeng';
+import { ContextMenuModule } from 'primeng/primeng';
+import { DataGridModule } from 'primeng/primeng';
+import { DataListModule } from 'primeng/primeng';
+import { DataScrollerModule } from 'primeng/primeng';
+import { DataTableModule } from 'primeng/primeng';
+import { DialogModule } from 'primeng/primeng';
+import { DragDropModule } from 'primeng/primeng';
+import { DropdownModule } from 'primeng/primeng';
+import { EditorModule } from 'primeng/primeng';
+import { FieldsetModule } from 'primeng/primeng';
+import { FileUploadModule } from 'primeng/primeng';
+import { GalleriaModule } from 'primeng/primeng';
+import { GMapModule } from 'primeng/primeng';
+import { GrowlModule } from 'primeng/primeng';
+import { InputMaskModule } from 'primeng/primeng';
+import { InputSwitchModule } from 'primeng/primeng';
+import { InputTextModule } from 'primeng/primeng';
+import { InputTextareaModule } from 'primeng/primeng';
+import { LightboxModule } from 'primeng/primeng';
+import { ListboxModule } from 'primeng/primeng';
+import { MegaMenuModule } from 'primeng/primeng';
+import { MenuModule } from 'primeng/primeng';
+import { MenubarModule } from 'primeng/primeng';
+import { MessagesModule } from 'primeng/primeng';
+import { MultiSelectModule } from 'primeng/primeng';
+import { OrderListModule } from 'primeng/primeng';
+import { OrganizationChartModule } from 'primeng/primeng';
+import { OverlayPanelModule } from 'primeng/primeng';
+import { PaginatorModule } from 'primeng/primeng';
+import { PanelModule } from 'primeng/primeng';
+import { PanelMenuModule } from 'primeng/primeng';
+import { PasswordModule } from 'primeng/primeng';
+import { PickListModule } from 'primeng/primeng';
+import { ProgressBarModule } from 'primeng/primeng';
+import { RadioButtonModule } from 'primeng/primeng';
+import { RatingModule } from 'primeng/primeng';
+import { ScheduleModule } from 'primeng/primeng';
+import { SelectButtonModule } from 'primeng/primeng';
+import { SlideMenuModule } from 'primeng/primeng';
+import { SliderModule } from 'primeng/primeng';
+import { SpinnerModule } from 'primeng/primeng';
+import { SplitButtonModule } from 'primeng/primeng';
+import { StepsModule } from 'primeng/primeng';
+import { TabMenuModule } from 'primeng/primeng';
+import { TabViewModule } from 'primeng/primeng';
+import { TerminalModule } from 'primeng/primeng';
+import { TieredMenuModule } from 'primeng/primeng';
+import { ToggleButtonModule } from 'primeng/primeng';
+import { ToolbarModule } from 'primeng/primeng';
+import { TooltipModule } from 'primeng/primeng';
+import { TreeModule } from 'primeng/primeng';
+import { TreeTableModule } from 'primeng/primeng';
+
+// Import Pages
+import {AppMenuComponent, AppSubMenuComponent} from './app.menu.component';
+import {AppSideBarComponent} from './app.sidebar.component';
+import {AppSidebartabcontentComponent} from './app.sidebartabcontent.component';
+import {AppTopbarComponent} from './app.topbar.component';
+import {AppFooterComponent} from './app.footer.component';
+
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ErrorComponent } from './error/error.component';
 import { EmptyComponent } from './empty/empty.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AppMenuComponent,
+    AppSubMenuComponent,
+    AppSideBarComponent,
+    AppSidebartabcontentComponent,
+    AppTopbarComponent,
+    AppFooterComponent,
     HomeComponent,
     LoginComponent,
     NotFoundComponent,
     ErrorComponent,
-    EmptyComponent
+    EmptyComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,8 +110,73 @@ import { EmptyComponent } from './empty/empty.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    ButtonModule
-    
+    ButtonModule,
+    BrowserAnimationsModule,
+    AccordionModule,
+    AutoCompleteModule,
+    BreadcrumbModule,
+    ButtonModule,
+    CalendarModule,
+    CarouselModule,
+    ColorPickerModule,
+    ChartModule,
+    CheckboxModule,
+    ChipsModule,
+    CodeHighlighterModule,
+    ConfirmDialogModule,
+    SharedModule,
+    ContextMenuModule,
+    DataGridModule,
+    DataListModule,
+    DataScrollerModule,
+    DataTableModule,
+    DialogModule,
+    DragDropModule,
+    DropdownModule,
+    EditorModule,
+    FieldsetModule,
+    FileUploadModule,
+    GalleriaModule,
+    GMapModule,
+    GrowlModule,
+    InputMaskModule,
+    InputSwitchModule,
+    InputTextModule,
+    InputTextareaModule,
+    LightboxModule,
+    ListboxModule,
+    MegaMenuModule,
+    MenuModule,
+    MenubarModule,
+    MessagesModule,
+    MultiSelectModule,
+    OrderListModule,
+    OrganizationChartModule,
+    OverlayPanelModule,
+    PaginatorModule,
+    PanelModule,
+    PanelMenuModule,
+    PasswordModule,
+    PickListModule,
+    ProgressBarModule,
+    RadioButtonModule,
+    RatingModule,
+    ScheduleModule,
+    SelectButtonModule,
+    SlideMenuModule,
+    SliderModule,
+    SpinnerModule,
+    SplitButtonModule,
+    StepsModule,
+    TabMenuModule,
+    TabViewModule,
+    TerminalModule,
+    TieredMenuModule,
+    ToggleButtonModule,
+    ToolbarModule,
+    TooltipModule,
+    TreeModule,
+    TreeTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
