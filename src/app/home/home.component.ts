3cd8e82ea958deace330fcc56e455d89c8842469
DESKTOP-OIU5JI7\Rizki <mr.rmdhn6@gmail.com>
2018-04-25 04:38:36 +0700

    sidebar gacor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,114 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Renderer, OnDestroy } from '@angular/core';
+
+enum MenuOrientation {
+    STATIC,
+    OVERLAY
+  }
 
 @Component({
-  selector: 'app-home',
-  template: `
-      <div class="layout-wrapper" [ngClass]="{'layout-wrapper-menu-active':sidebarActive,
-                                          'layout-overlay-menu':overlay}">
-
-        <app-topbar></app-topbar>
-
-        <app-sidebar></app-sidebar>
-
-        <div class="layout-main">
-            <div class="layout-main-content">
-                <router-outlet></router-outlet>
-
-                <app-footer></app-footer>
-
-            </div>
-        </div>
-
-    </div>
-  `,
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
-export class HomeComponent { }
+export class HomeComponent implements AfterViewInit, OnDestroy {
+
+    activeTabIndex: number;
+
+    sidebarActive: boolean;
+
+    layoutMode: MenuOrientation = MenuOrientation.STATIC;
+
+    darkMenu = false;
+
+    topbarMenuActive: boolean;
+
+    sidebarClick: boolean;
+
+    topbarItemClick: boolean;
+
+    activeTopbarItem: any;
+
+    documentClickListener: Function;
+
+    constructor(
+        public renderer: Renderer
+    ) {
+    }
+
+    ngAfterViewInit() {
+        this.documentClickListener = this.renderer.listenGlobal('body', 'click', (event) => {
+            if (!this.topbarItemClick) {
+                this.activeTopbarItem = null;
+                this.topbarMenuActive = false;
+            }
+
+            if (!this.sidebarClick && (this.overlay || !this.isDesktop())) {
+                this.sidebarActive = false;
+            }
+
+            this.topbarItemClick = false;
+            this.sidebarClick = false;
+        });
+    }
+
+    onTabClick(event: Event, index: number) {
+        if (this.activeTabIndex === index) {
+            this.sidebarActive = !this.sidebarActive;
+        } else {
+            this.activeTabIndex = index;
+            this.sidebarActive = true;
+        }
+
+        event.preventDefault();
+    }
+
+    closeSidebar(event: Event) {
+        this.sidebarActive = false;
+        event.preventDefault();
+    }
+
+    onSidebarClick($event) {
+        this.sidebarClick = true;
+    }
+
+    onTopbarMenuButtonClick(event) {
+        this.topbarItemClick = true;
+        this.topbarMenuActive = !this.topbarMenuActive;
+
+        event.preventDefault();
+    }
+
+    onTopbarItemClick(event, item) {
+        this.topbarItemClick = true;
+
+        if (this.activeTopbarItem === item) {
+            this.activeTopbarItem = null;
+        } else {
+            this.activeTopbarItem = item;
+        }
+
+        event.preventDefault();
+    }
+
+    get overlay(): boolean {
+        return this.layoutMode === MenuOrientation.OVERLAY;
+    }
+
+    changeToStaticMenu() {
+        this.layoutMode = MenuOrientation.STATIC;
+    }
+
+    changeToOverlayMenu() {
+        this.layoutMode = MenuOrientation.OVERLAY;
+    }
+
+    isDesktop() {
+        return window.innerWidth > 1024;
+    }
+
+    ngOnDestroy() {
+        if (this.documentClickListener) {
+            this.documentClickListener();
+        }
+    }
+}
--- src/service/shared/config.ts
+++ src/service/shared/config.ts
@@ -1,5 +1,5 @@
 export class CONFIGGLOBAL {
-    public static API_URL = 'http://192.168.1.45/apiresti/api';
+    public static API_URL = 'http://192.168.1.74/apiresti/api';
 }
 
 // resti/api/auth/login
