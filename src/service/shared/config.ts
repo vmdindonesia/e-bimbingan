30381eea136f2ee6b444562ce586d20762f610f7
REN <rendy1429@gmail.com>
2018-04-26 01:17:14 +0700

    change url to development

--- src/service/shared/config.ts
+++ src/service/shared/config.ts
@@ -1,5 +1,5 @@
 export class CONFIGGLOBAL {
-    public static API_URL = 'http://192.168.1.74/apiresti/api';
+    public static API_URL = 'http://apiresti.vmdindonesia.com/api';
 }
 
 // resti/api/auth/login
