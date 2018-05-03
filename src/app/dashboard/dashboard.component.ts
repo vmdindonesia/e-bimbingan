ab51cfb7f26449a3c3489c319997c771da43e0e9
DESKTOP-OIU5JI7\Rizki <mr.rmdhn6@gmail.com>
2018-05-04 05:04:01 +0700

    update view and design

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  chartData: any;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.chartData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: '#FFC107'
+        },
+        {
+          label: 'Second Dataset',
+          data: [28, 48, 40, 19, 86, 27, 90],
+          fill: false,
+          borderColor: '#03A9F4'
+        }
+      ]
+    };
+
   }
 
 }
